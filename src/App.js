bc3f0ba79018333c39ab5271f58760c3d5cbcb13
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-06-01 22:31:14 -0700

    added in roll button and creating dices that save in an array in state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Die from "./components/Die.js";
+import { nanoid } from "nanoid";
 
 function App() {
+  // store your dices in state, initial state will call allNewDice to creat them
+  const [dices, setDices] = useState(allNewDice());
+
   // function to create a new array of 10 items with values from 1-6
+  // not sure why but when I tried creating the object first instead of within the for loop I couldn't push a new dice and would repeat the first one
   function allNewDice() {
     const array = [];
-
     for (let i = 0; i < 10; i++) {
-      array.push(Math.ceil(Math.random() * 6));
+      array.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
     return array;
+  }
+
+  // render dice Elements here
+  const diceElements = dices.map((dice) => (
+    <Die key={dice.id} value={dice.value} />
+  ));
+
+  // for our Roll button, it generates new dices for us and passes them into setDices
+  function rollDice() {
+    setDices(allNewDice());
   }
 
   return (
     <div className="App">
       <main>
-        <div className="dice-container">
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-        </div>
+        <div className="dice-container">{diceElements}</div>
+        <button className="roll-button" onClick={rollDice}>
+          Roll
+        </button>
       </main>
     </div>
   );
