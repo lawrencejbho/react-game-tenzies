9abda5eaed310f9cb2413881d05bab7c5532f801
Lawrence Ho <lawrence.jb.ho@gmail.com>
2022-06-05 23:05:30 -0700

    added holdDice functionality, state with tenzies to check for win, render new game and confetti when winning, new game

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Die from "./components/Die.js";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 function App() {
   // store your dices in state, initial state will call allNewDice to creat them
   const [dices, setDices] = useState(allNewDice());
 
-  // function to create a new array of 10 items with values from 1-6
+  const [tenzies, setTenzies] = useState(false);
+
+  // this helps with keeping up with two things that have state and we can monitor our dices on each change
+  // every method is nice to check that each item in the array has isHeld
+  // we use the firstValue of the first item in the array to help us check that value versus every item in the array
+  useEffect(() => {
+    const allHeld = dices.every((dice) => dice.isHeld);
+    const firstValue = dices[0].value;
+    const allSameValue = dices.every((dice) => dice.value == firstValue);
+
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    } else {
+      console.log("state changed");
+    }
+  }, [dices]);
+
+  // use this function anytime we need a new Dice
+  function generateNewDice() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  // function to create a new array of 10 items with values from 1-6 for the start of the game
   // not sure why but when I tried creating the object first instead of within the for loop I couldn't push a new dice and would repeat the first one
   function allNewDice() {
     const array = [];
     for (let i = 0; i < 10; i++) {
-      array.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
+      array.push(generateNewDice());
     }
     return array;
   }
 
+  // if the id matches, we flip isHeld
   function holdDice(id) {
-    console.log(id);
+    setDices((prevDice) =>
+      prevDice.map((dice) => {
+        return id === dice.id ? { ...dice, isHeld: !dice.isHeld } : dice;
+      })
+    );
+  }
+
+  // Roll button - we check the isHeld is true and generate a new dice if it's false
+  // check if tenzies is true, if it is then run allNewDice otherwise check for held dice
+  function rollDice() {
+    if (tenzies) {
+      setDices(allNewDice());
+      setTenzies(false);
+    } else {
+      setDices((prevDice) =>
+        prevDice.map((dice) => (dice.isHeld ? dice : generateNewDice()))
+      );
+    }
   }
 
   // render dice Elements here
@@ -30,21 +71,25 @@ function App() {
       key={dice.id}
       value={dice.value}
       isHeld={dice.isHeld}
+      // we pass the dice.id parameter directly into it so needs to be written with this syntax
       holdDice={() => holdDice(dice.id)}
     />
   ));
 
-  // for our Roll button, it generates new dices for us and passes them into setDices
-  function rollDice() {
-    setDices(allNewDice());
-  }
+  const bottomButton = tenzies ? "New Game" : "Roll";
 
   return (
     <div className="App">
       <main>
+        {tenzies && <Confetti />}
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
         <div className="dice-container">{diceElements}</div>
         <button className="roll-button" onClick={rollDice}>
-          Roll
+          {bottomButton}
         </button>
       </main>
     </div>
